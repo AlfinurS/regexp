30d8f4f4c73c2492205aeff8131fccfb7ab78afe
AlfinurS <aaalfinur87@gmail.com>
2023-03-16 18:41:52 +0300

    Валидация полей

--- index.js
+++ index.js
@@ -19,59 +19,48 @@ class TextRegExp{
 addEventListener("input", (e) => {
   if(e.target.classList.contains('registration__form-input')){
     const idInput = e.target.id;
-    if(idInput === "name"){
-      isText(e.target.value);
-    }
-    if(idInput === "email"){
-      isEmail(e.target.value);
-    }
-    if(idInput === "phone"){
-      isPhone(e.target.value);
-    }
+    isText(e.target.value, idInput);
   }
 });
 
 const textRegex = /^[а-яА-ЯёЁa-zA-Z0-9\s-_,@"'\.]+$/;
-const isText = (val) => {
-  let isNameCorrect = textRegex.test(val);
-    if(isNameCorrect === false){
-      const block = document.getElementById("name");
-      block.classList.add('registration__error');
-      const formaBlock = document.querySelector('.registration__form-inputs');
-      const textError = `<p class="registration__error">Ошибка!</p>`;
-      formaBlock.insertAdjacentHTML('afterbegin',textError);
-    }
-    return
-};
-
 
 const emailRegEx =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9А-ЯЁа-яё]+\.)+[a-zA-ZА-ЯЁа-яё]{2,}))$/;
 
-  const isEmail = (val) => {
-    let isEmailCorrect = emailRegEx.test(val);
-      if(isEmailCorrect === false){
-        const block = document.getElementById("email");
-        block.classList.add('registration__error');
-        const formaBlock = document.querySelector('.registration__form-inputs');
-        const textError = `<p class="registration__error">Ошибка!</p>`;
-        formaBlock.insertAdjacentHTML('afterbegin',textError);
-      }
-      return
-  };
-  
-
-
 const phoneRegex = /^.?.?.?.?\d{3}.?.?\d{3}.?.?.?\d{2}.?.?.?\d{2}$/;
 
-const isPhone = (val) => {
-  let isPhoneCorrect = phoneRegex.test(val);
-    if(isPhoneCorrect === false){
-      const block = document.getElementById("phone");
+const validateText = (val) => {
+  return textRegex.test(val);
+}
+
+const validateEmail = (val) => {
+  return emailRegEx.test(val);
+}
+
+const validatePhone = (val) => {
+  return phoneRegex.test(val);
+}
+
+const validatorsMap = {
+  name: validateText,
+  email: validateEmail,
+  phone: validatePhone,
+}
+
+
+const isText = (val, key) => {
+  let isFieldCorrect = validatorsMap[key](val);
+    if(isFieldCorrect === false){
+      const block = document.getElementById(key);
       block.classList.add('registration__error');
-      const formaBlock = document.querySelector('.registration__form-inputs');
-      const textError = `<p class="registration__error">Ошибка!</p>`;
-      formaBlock.insertAdjacentHTML('afterbegin',textError);
+      const wrapper = document.getElementById(`wrapper-${key}`);
+      wrapper.classList.add('has-error');
+    } else {
+      const block = document.getElementById(key);
+      block.classList.remove('registration__error');
+      const wrapper = document.getElementById(`wrapper-${key}`);
+      wrapper.classList.remove('has-error');
     }
     return
 };
